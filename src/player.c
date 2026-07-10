1eafa995f5c4a15c2a89dc8214f9aeaed193bb5b
sabrinamaimon <sabrinamihiya@gmail.com>
2026-07-10 23:03:11 +0600

    3 NO UPDATE

--- src/player.c
+++ src/player.c
@@ -29,4 +29,32 @@ void Player_Update(Player *p,Rectangle roomBounds,float dt)
 
     float targetSpeedX=wantX*p->speed;
     float targetSpeedY=wantY*p->speed;
+
+    /*smoothly move current speed 
+    changing 0.15f-bigger=snappier,smaller=floater*/
+
+    float nudgeAmount=0.15f;
+    currentspeedX=currentSpeedX+(targetSpeedX-currentSpeedX)*nudgeAmount;
+    currentSpeedY=currentSpeedY+(targetSpeedY-currentSpeedY)*nudgeAmount;
+
+    /*moving the player */
+    p->position.x+=currentSpeedX*dt;
+    p->position.y+=currentSpeedY*dt;
+
+    /*not letting the player leave the room*/
+
+    if(p->position.x<roomBounds.x)
+    p->position.x=roomBounds.x;
+    if(p->position.y<roomBounds.y)
+    p->position.y=roomBounds.y;
+    if(p->position.x+p->size.x >roomBounds.x+roomBounds.width)
+    p->position.x=roomBounds.x+roomBounds.width - p->size.x;
+    if(p->position.y +p->size.y>roomBounds.y +roomBounds.height)
+    p->position.y=roomBounds.y+roomBounds.height - p->size.y;
+
+}
+void Player_Draw(Player *p)
+{
+    DrawRectangleV(p->position,p->size,SKYBLUE);
+    DrawRectangleLines((int)p->position.x,(int)p->position.y,(int)p->size.x,(int)p->size.y,DARKBLUE);
 }
