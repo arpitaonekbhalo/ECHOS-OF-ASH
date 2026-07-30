4ab5182c86057e31fcfd6c2628bd60665622851a
arpitaonekbhalo <jannatmiftahul837@gmail.com>
2026-07-31 02:44:10 +0600

    Update zombie.h

--- src/zombie.h
+++ src/zombie.h
@@ -1,7 +1,9 @@
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
 #include "game.h"
-void Zombie_Spawn(Zombie z[], int *count, Vector2 pos, float speed);
-void Zombie_UpdateAll(Zombie z[], int count, Player *player, Rectangle roomBounds, float dt);
+
+void Zombie_Spawn(Zombie z[], int *count, Vector3 pos, float speed);
+void Zombie_UpdateAll(Zombie z[], int count, Player *player, Vector3 roomMin, Vector3 roomMax, float dt);
 void Zombie_DrawAll(Zombie z[], int count);
+
 #endif
