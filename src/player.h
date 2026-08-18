f3e0e043bd6fe2b9b38bdb544120277e4a37cfd2
sabrinamaimon <sabrinamihiya@gmail.com>
2026-08-18 23:13:03 +0600

    some del

--- src/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-#include "game.h"
-
-void Player_Init(Player *p, Vector3 startPos);
-void Player_Update(Player *p,Vector3 roomMin,Vector3 roomMax, float dt);
-void Player_Draw(Player *p);
-
-#endif
